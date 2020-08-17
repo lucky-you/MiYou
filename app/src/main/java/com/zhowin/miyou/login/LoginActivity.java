c0b8ec19c262500c8d6545d48a96d9f901540b89
周彬 <1021237228@qq.com>
2020-08-17 16:31:29 +0800

    forget password

--- app/src/main/java/com/zhowin/miyou/login/LoginActivity.java
+++ app/src/main/java/com/zhowin/miyou/login/LoginActivity.java
@@ -1,6 +1,8 @@
 package com.zhowin.miyou.login;
 
 
+import android.view.View;
+
 import com.zhowin.base_library.base.BaseBindActivity;
 import com.zhowin.miyou.R;
 import com.zhowin.miyou.databinding.ActivityLoginBinding;
@@ -15,11 +17,21 @@ public class LoginActivity extends BaseBindActivity<ActivityLoginBinding> {
 
     @Override
     public void initView() {
-
+        setOnClick(R.id.tvForgetPassword);
     }
 
     @Override
     public void initData() {
 
+    }
+
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.tvForgetPassword:
+                startActivity(ForgetPasswordActivity.class);
+                break;
+        }
     }
 }
