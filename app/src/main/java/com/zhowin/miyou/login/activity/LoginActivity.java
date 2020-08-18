45367991ffa2497a3bdffe3019ae73793752291a
周彬 <1021237228@qq.com>
2020-08-18 20:21:04 +0800

    change  ui

--- app/src/main/java/com/zhowin/miyou/login/activity/LoginActivity.java
+++ app/src/main/java/com/zhowin/miyou/login/activity/LoginActivity.java
@@ -2,6 +2,8 @@ package com.zhowin.miyou.login.activity;
 
 
 import android.graphics.Typeface;
+import android.text.method.HideReturnsTransformationMethod;
+import android.text.method.PasswordTransformationMethod;
 import android.view.View;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
@@ -15,8 +17,10 @@ import com.zhowin.miyou.databinding.ActivityLoginBinding;
 /**
  * 登录
  */
-public class LoginActivity extends BaseBindActivity<ActivityLoginBinding> implements RadioGroup.OnCheckedChangeListener {
+public class LoginActivity extends BaseBindActivity<ActivityLoginBinding> {
 
+
+    private boolean isShowPassword;
 
     @Override
     public int getLayoutId() {
@@ -25,13 +29,14 @@ public class LoginActivity extends BaseBindActivity<ActivityLoginBinding> implem
 
     @Override
     public void initView() {
-        setOnClick(R.id.tvForgetPassword, R.id.tvGetVerificationCode, R.id.tvLogin, R.id.tvOneClickLogin);
-        mBinding.rgLoginButton.setOnCheckedChangeListener(this::onCheckedChanged);
+        setOnClick(R.id.rlSMSLoginLayout, R.id.rlPasswordLayout, R.id.tvForgetPassword,
+                R.id.tvGetVerificationCode, R.id.tvLogin, R.id.tvOneClickLogin,
+                R.id.ivPasswordClose, R.id.ivWeChatLogin, R.id.ivQQLogin);
     }
 
     @Override
     public void initData() {
-
+        changeUiOfClickStatus(true);
     }
 
 
@@ -46,42 +51,71 @@ public class LoginActivity extends BaseBindActivity<ActivityLoginBinding> implem
     @Override
     public void onClick(View v) {
         switch (v.getId()) {
+            case R.id.rlSMSLoginLayout:
+                changeUiOfClickStatus(true);
+                break;
+            case R.id.rlPasswordLayout:
+                changeUiOfClickStatus(false);
+                break;
             case R.id.tvForgetPassword:
                 startActivity(ForgetPasswordActivity.class);
                 break;
             case R.id.tvGetVerificationCode:
-                startActivity(BindPhoneActivity.class);
                 break;
             case R.id.tvLogin:
-//                startActivity(ForgetPasswordActivity.class);
                 break;
             case R.id.tvOneClickLogin:
                 startActivity(EditNickNameActivity.class);
                 break;
-        }
-    }
-
-    @Override
-    public void onCheckedChanged(RadioGroup group, int checkedId) {
-        switch (checkedId) {
-            case R.id.rbPhoneLogin:
-                mBinding.llPhoneLayout.setVisibility(View.VISIBLE);
-                mBinding.llPasswordLayout.setVisibility(View.GONE);
+            case R.id.ivPasswordClose:
+                isShowPassword = !isShowPassword;
+                setShowPassword();
                 break;
-            case R.id.rbPasswordLogin:
-                mBinding.llPhoneLayout.setVisibility(View.GONE);
-                mBinding.llPasswordLayout.setVisibility(View.VISIBLE);
+            case R.id.ivWeChatLogin:
+                startActivity(BindPhoneActivity.class);
+                break;
+            case R.id.ivQQLogin:
                 break;
         }
     }
 
-    private void setBottomStatusUi(boolean isSelect, RadioButton radioButton) {
-        radioButton.setTypeface(radioButton.isChecked() ? Typeface.defaultFromStyle(Typeface.BOLD) : Typeface.defaultFromStyle(Typeface.NORMAL));
-        radioButton.setTextColor(radioButton.isChecked() ? getBaseColor(R.color.color_333) : getBaseColor(R.color.color_666));
-        radioButton.setTextSize(radioButton.isChecked() ? 23 : 16);
 
-//        SetDrawableResourceHelper.setBottomDrawable(mContext,radioButton, isSelect, 0, 0);
+    private void setShowPassword() {
+        if (isShowPassword) {
+            mBinding.editPassword.setTransformationMethod(HideReturnsTransformationMethod.getInstance());
+            mBinding.ivPasswordClose.setImageResource(R.mipmap.login_eyeopen_icon);
+        } else {
+            mBinding.editPassword.setTransformationMethod(PasswordTransformationMethod.getInstance());
+            mBinding.ivPasswordClose.setImageResource(R.mipmap.login_eyeclose_icon);
+        }
+        mBinding.editPassword.setSelection(mBinding.editPassword.getText().toString().trim().length());
+    }
 
+
+    private void changeUiOfClickStatus(boolean isClickSMSLoginLayout) {
+        if (isClickSMSLoginLayout) {
+            mBinding.tvSMSLogin.setTextColor(getBaseColor(R.color.color_333));
+            mBinding.ivSMSBottomView.setVisibility(View.VISIBLE);
+
+            mBinding.tvPasswordLogin.setTextColor(getBaseColor(R.color.color_666));
+            mBinding.ivPasswordBottomView.setVisibility(View.GONE);
+
+            mBinding.llPhoneLayout.setVisibility(View.VISIBLE);
+            mBinding.llPasswordLayout.setVisibility(View.GONE);
+            mBinding.tvForgetPassword.setVisibility(View.INVISIBLE);
+
+        } else {
+            mBinding.tvPasswordLogin.setTextColor(getBaseColor(R.color.color_333));
+            mBinding.ivPasswordBottomView.setVisibility(View.VISIBLE);
+
+            mBinding.tvSMSLogin.setTextColor(getBaseColor(R.color.color_666));
+            mBinding.ivSMSBottomView.setVisibility(View.GONE);
+
+            mBinding.llPhoneLayout.setVisibility(View.GONE);
+            mBinding.llPasswordLayout.setVisibility(View.VISIBLE);
+            mBinding.tvForgetPassword.setVisibility(View.VISIBLE);
+
+        }
     }
 
 }
